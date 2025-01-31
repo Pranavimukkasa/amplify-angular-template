9388016a63f49f27bcac83b9767a1d0330dbb4ea
Pranavi M <1000071640@hexaware.com>
2025-01-31 18:27:52 +0530

    removed amplify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
-import outputs from '../../amplify_outputs.json';
 import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
 
-Amplify.configure(outputs);
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, TodosComponent, AmplifyAuthenticatorModule],
+  imports: [RouterOutlet, AmplifyAuthenticatorModule],
 
   
 })
@@ -20,7 +18,7 @@ export class AppComponent {
   title = 'amplify-angular-template';
 
     
-  constructor(public authenticator: AuthenticatorService) {
-    Amplify.configure(outputs);
-  }
+  // constructor(public authenticator: AuthenticatorService) {
+  //   Amplify.configure(outputs);
+  // }
 }
